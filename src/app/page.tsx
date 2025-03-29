5b01f533e03e12426dc055eca43dfdd4f10b2a8b
Hector Ligsay Jr <hligsay@aol.com>
2025-03-29 09:24:38 -0500

    Fixed lightbulb icon bc I forgot to save it. Anchored watermark closer to the delete last offer button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import HomePage from "@/components/home/home-page"
 
 export default function Home() {
-  return <HomePage />
+  return (
+    <main className="min-h-screen p-4 max-w-2xl mx-auto relative">
+      <HomePage />
+    </main>
+  );
 }
 
