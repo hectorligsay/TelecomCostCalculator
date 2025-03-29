56d7ea1793c6f4d3607dd6baf93f45a96d41ddfa
Hector Ligsay Jr <hligsay@aol.com>
2025-03-29 09:10:31 -0500

    Added business watermark and fixed the emojis so Annual Box can fit.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
+'use client'
+
 import HomePage from "@/components/home/home-page"
 
 export default function Home() {
-  return <HomePage />
+  return (
+    <main className="min-h-screen p-4 max-w-2xl mx-auto relative">
+      <HomePage />
+      <div className="absolute bottom-2 right-2 text-gray-400 text-sm">
+        Precision Solutions HL™
+      </div>
+    </main>
+  );
 }
 
